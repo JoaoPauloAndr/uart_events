30980cab40cbd3ca2fbc0c5e199a73bbf0400973
JoaoPauloAndr <p.paulo.andrade@gmail.com>
2022-03-08 17:56:06 -0300

    Primeiro commit

---
+++ components/at_cmd_processing/at_cmd_processing.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include "at_cmd_processing.h"
+
+int getIndex(char* str, char key)
+{
+    char *e;
+    e = strchr(str, key);
+    return (int)(e - str);
+}
+
+enum input_type checkInput(char *buff, int buff_len, int* return_index)
+{
+    if(buff_len < 4)
+        return Input_Error;
+
+    //verificar se é comando AT
+    char at_cmd[3];
+    strncpy(at_cmd ,buff, 3);
+    int check = strcmp("AT+", at_cmd);
+    if(check != 0) 
+        return Input_Error; 
+
+    int equalsIndex = getIndex(buff, '=');
+    enum input_type num_code = Input_Error;
+    if(equalsIndex < buff_len && equalsIndex > 3){
+        int command_len = equalsIndex-3; 
+        char command[equalsIndex-3];
+        strncpy(command, buff+3, command_len);
+        command[command_len] = '\0';
+        num_code = checkNumCode(command);
+        *return_index = equalsIndex;
+    }
+    return num_code;      
+}
+
+
+// void writeToLog(char *log, int code){
+//     FILE *fp;
+//     time_t t = time(NULL);
+//     struct tm tm = *localtime(&t);
+//     char logfile_name[LOGFILE_NAME_SIZE];
+//     sprintf(logfile_name, "log%02d-%02d-%d.log", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+//     if((fp = fopen(logfile_name , "a+")) == NULL) {
+//         printf("Failed to open log file.\n");
+//         return;
+//     }else{
+//         switch (code)
+//         {
+//         case REVERSE:
+//             fprintf(fp, "[AT+REVERSE]\n");
+//             fprintf(fp, "%02d:%02d:%02d:\t", tm.tm_hour, tm.tm_min, tm.tm_sec);
+//             fprintf(fp, "%s", log);
+//             break;
+//         case SIZE:
+//             fprintf(fp, "[AT+SIZE]\n");
+//             fprintf(fp, "%02d:%02d:%02d:\t", tm.tm_hour, tm.tm_min, tm.tm_sec);
+//             fprintf(fp, "%s", log);
+//             break;    
+//         case MULT:
+//             fprintf(fp, "[AT+MULT]\n");
+//             fprintf(fp, "%02d:%02d:%02d:\t", tm.tm_hour, tm.tm_min, tm.tm_sec);
+//             fprintf(fp, "%s", log);
+//             break;
+//         default:
+//             fprintf(fp, "[ERROR]\n");
+//             fprintf(fp, "%02d:%02d:%02d:\t", tm.tm_hour, tm.tm_min, tm.tm_sec);
+//             fprintf(fp, "%s", log);
+//             break;
+//         }
+//     }
+//     fclose(fp);   
+// }
+
+enum input_type checkNumCode(char *command)
+{
+    int reverse = strcmp(REVERSE_COMMAND, command);
+    int size = strcmp(SIZE_COMMAND, command);
+    int mult = strcmp(MULT_COMMAND, command);
+    if(reverse == 0)
+    {
+        return Reverse_Cmd;
+    }
+    else if(size == 0)
+    {
+        return Size_Cmd;
+    }
+    else if(mult == 0)
+    {
+        return Mult_Cmd;
+    }
+    else
+    {
+        return Input_Error;
+    }
+}
+
+
+void getString(char* buff, int equalsIndex, int len, char* returnStr, int* error){
+    //verificar se string termina no =
+    int quoteIndex = equalsIndex+1; 
+    if(len == (quoteIndex) || buff[quoteIndex] != '"'){
+        *error = 1;
+    }else{
+        int input_len = len-quoteIndex;
+        if(input_len < 2){
+            *error = 1;
+        }else{
+            char in[input_len];
+            strncpy(in, buff+(quoteIndex+1), input_len); //elimina a primeira "
+            in[input_len] = '\0';
+            int secondQuoteIndex = getIndex(in, '"');
+            if(secondQuoteIndex < 0 || secondQuoteIndex > input_len){
+                *error = 1;
+            }else{
+                //Entrada ok
+                strncpy(returnStr, in, secondQuoteIndex);
+                returnStr[secondQuoteIndex] = '\0';
+                *error = 0;
+            }
+        }
+    }
+
+}
+
+void reverse_str(char* msg, int equalsIndex, int len, char* output, int* error)
+{
+    getString(msg, equalsIndex, len, output, error);
+    if(*error == 1)
+    {
+        return;
+    }
+    else
+    {
+        int new_len = strlen(output);
+        int i, temp;
+        for(i = 0; i < new_len/2; i++)
+        {
+            temp = output[i];
+            output[i] = output[new_len-i-1];
+            output[new_len-i-1] = temp;
+        }
+    }
+}
+
+size_t size(char* msg, int equalsIndex, int len, int* error)
+{
+    size_t buffsize = len-8;
+    char input[buffsize];
+    getString(msg, equalsIndex, len, input, error);
+    if(*error)
+    {
+        return 0;
+    }
+    else
+    {
+        return strlen(input);
+    }
+}
+
+int getNumbers(char* buff, int startingIndex, int len, int* reachedComma, int* error){
+    if(startingIndex == len){
+        *error = 1;
+        return -1;
+    }else{
+        int i;
+        int empty = 1;
+        for(i = startingIndex; i < len; i++){
+            char character = buff[i];
+            if(isdigit(character)){
+                empty = 0;
+                continue;
+            }else if(isalpha(character)){
+                *error = 1;
+                return -1;
+            }else{
+                if(character == '.'){
+                    *error = 1;
+                    return -1;
+                }else if(character == ',' && *reachedComma == 0 && empty == 0){
+                    *reachedComma = i;
+                    break;
+                }else{
+                    *error = 1;
+                    return -1;
+                }
+            }
+        }
+        int num_len = i - startingIndex;
+        char num_str[num_len];
+        strncpy(num_str, buff+startingIndex, num_len);
+        num_str[num_len] = '\0';
+        return atoi(num_str);
+    }
+}
+
+int mult(char* msg, int equalsIndex, int len, int* error)
+{
+    int reachedComma = 0;
+    int a = getNumbers(msg, equalsIndex+1, len, &reachedComma, error);
+    if(*error)
+    {
+        return -1;
+    }
+    else
+    {
+        int b = getNumbers(msg, reachedComma+1, len, &reachedComma, error);
+        if(*error)
+        {
+            return -1;
+        }
+        else
+        {
+            return a * b;
+        }
+        
+    }
+}
---
+++ main/uart_events_example_main.c
@@ -0,0 +1,282 @@
+/* UART Events Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/timers.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "at_cmd_processing.h"
+
+static const char *TAG = "uart_events";
+
+/**
+ * This example shows how to use the UART driver to handle special UART events.
+ *
+ * It also reads data from UART0 directly, and echoes it to console.
+ *
+ * - Port: UART0
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: on
+ * - Pin assignment: TxD (default), RxD (default)
+ */
+
+/**
+TODO:
+* - implementar mutex no buffer!!
+* - corrigir tratamento dados
+* - reformular at_cmd_processing 
+* - Tratar dados recebidos
+*
+*/
+
+#define EX_UART_NUM UART_NUM_0
+#define PATTERN_CHR_NUM         (3)         /*!< Set the number of consecutive and identical characters received by receiver which defines a UART pattern*/
+#define BUF_SIZE                (1024)
+#define RD_BUF_SIZE             (BUF_SIZE)
+#define FIXED_TIMER             200000
+
+static QueueHandle_t uart0_queue;
+QueueHandle_t messageQueue; 
+char dtcpy[RD_BUF_SIZE];
+volatile char buffer[RD_BUF_SIZE];
+volatile unsigned int DATA_IN              = 0;
+volatile unsigned int RECEIVING_PACKAGE    = 0;
+volatile unsigned int CURRENT_PACKAGE_SIZE = 0;
+volatile unsigned int LAST_PACKAGE_SIZE    = 0;
+TaskHandle_t process_buffer_Handle         = NULL;
+int baud_rate = 115200;
+
+static void process_buffer_task(void *pvParameters)
+{
+    char message_received[RD_BUF_SIZE]; 
+    for(;;)
+    {
+        /* Wait to receive a notification sent directly to this task from the
+        interrupt service routine. */
+        if(xQueueReceive(messageQueue, &message_received, (portTickType)portMAX_DELAY)) 
+        {
+            int msg_len = strlen(message_received);
+            int return_index = 0;
+            int error = 0;
+            enum input_type num_code = checkInput(message_received, msg_len, &return_index);
+            switch (num_code)
+            {
+            case Reverse_Cmd:
+                printf("AT+REVERSE=\n");
+                char* output = calloc(msg_len-11, sizeof(char));
+                reverse_str(message_received, return_index, msg_len, output, &error);
+                if(error)
+                {
+                    printf("Erro no input.\n");
+                }
+                else
+                {
+                    printf("Entrada invertida = %s.\n", output);
+                }
+                free(output);
+                break;
+            case Size_Cmd:
+                printf("AT+SIZE=\n");
+                size_t input_size = size(message_received, return_index, msg_len, &error);
+                if(error)
+                {
+                    printf("Erro no input.\n");
+                }
+                else
+                {
+                    printf("String de tamanho = %zu.\n", input_size);
+                }
+                break;
+            case Mult_Cmd:
+                printf("AT+MULT=\n");
+                int result = mult(message_received, return_index, msg_len, &error);
+                if(error)
+                {
+                    printf("Erro no input.\n");
+                }
+                else
+                {
+                    printf("Resultado da multiplicacao = %d.\n", result);
+                }
+                break;    
+            default:
+                printf("Erro no comando.\n");
+                break;
+            }
+            memset(message_received, 0, sizeof(message_received));
+        }
+    }    
+    vTaskDelete(NULL);
+}
+
+void vTimerCallback()
+{
+    BaseType_t xStatus;
+    xStatus = xQueueSendToBack(messageQueue, &buffer, 0);
+    if(xStatus != pdPASS)
+    {
+        printf("Nao foi possivel acrescentar item a fila.\n");
+    } 
+    for(int i = 0; i < LAST_PACKAGE_SIZE; i++)
+    {
+        buffer[i] = 0;
+    }
+    RECEIVING_PACKAGE = 0;
+}
+
+static void uart_event_task(void *pvParameters)
+{
+    esp_timer_handle_t oneshot_timer = (esp_timer_handle_t) pvParameters;
+    uart_event_t event;
+    size_t buffered_size;
+    uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
+    for(;;) {
+        //Waiting for UART event.
+        if(xQueueReceive(uart0_queue, (void * )&event, (portTickType)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            //ESP_LOGI(TAG, "uart[%d] event:", EX_UART_NUM);
+            switch(event.type) {
+                //Event of UART receving data
+                /*We'd better handler data event fast, there would be much more data events than
+                other types of events. If we take too much time on data event, the queue might
+                be full.*/
+                case UART_DATA:
+                    DATA_IN = 1;
+                    //ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
+                    uart_read_bytes(EX_UART_NUM, dtmp, event.size, portMAX_DELAY);
+                    for(int i = 0; i < event.size; i++)
+                    {
+                        dtcpy[i] = (char*) dtmp[i];
+                    }
+                    if(strlen(buffer) < RD_BUF_SIZE)
+                        strcat(buffer, dtcpy);
+                    memset(dtcpy, 0, event.size); 
+                    CURRENT_PACKAGE_SIZE += event.size;  
+                    
+                    //ESP_LOGI(TAG, "[DATA EVT]:");
+                    //uart_write_bytes(EX_UART_NUM, (const char*) dtmp, event.size);
+                    break;
+                //Event of HW FIFO overflow detected
+                case UART_FIFO_OVF:
+                    //ESP_LOGI(TAG, "hw fifo overflow");
+                    // If fifo overflow happened, you should consider adding flow control for your application.
+                    // The ISR has already reset the rx FIFO,
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(EX_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+                //Event of UART ring buffer full
+                case UART_BUFFER_FULL:
+                    //ESP_LOGI(TAG, "ring buffer full");
+                    // If buffer full happened, you should consider encreasing your buffer size
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(EX_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+                //Event of UART RX break detected
+                case UART_BREAK:
+                    break;
+                //Event of UART parity check error
+                case UART_PARITY_ERR:
+                    break;
+                //Event of UART frame error
+                case UART_FRAME_ERR:
+                    //ESP_LOGI(TAG, "uart frame error");
+                    break;
+                //UART_PATTERN_DET
+                case UART_PATTERN_DET:
+                    uart_get_buffered_data_len(EX_UART_NUM, &buffered_size);
+                    int pos = uart_pattern_pop_pos(EX_UART_NUM);
+                    //ESP_LOGI(TAG, "[UART PATTERN DETECTED] pos: %d, buffered size: %d", pos, buffered_size);
+                    if (pos == -1) {
+                        // There used to be a UART_PATTERN_DET event, but the pattern position queue is full so that it can not
+                        // record the position. We should set a larger queue size.
+                        // As an example, we directly flush the rx buffer here.
+                        uart_flush_input(EX_UART_NUM);
+                    } else {
+                        uart_read_bytes(EX_UART_NUM, dtmp, pos, 100 / portTICK_PERIOD_MS);
+                        uint8_t pat[PATTERN_CHR_NUM + 1];
+                        memset(pat, 0, sizeof(pat));
+                        uart_read_bytes(EX_UART_NUM, pat, PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
+                        //ESP_LOGI(TAG, "read data: %s", dtmp);
+                        //ESP_LOGI(TAG, "read pat : %s", pat);
+                    }
+                    break;
+                default:
+                    break;    
+            }
+            if(DATA_IN == 1 && RECEIVING_PACKAGE == 0)
+            {
+                RECEIVING_PACKAGE = 1;
+                LAST_PACKAGE_SIZE = CURRENT_PACKAGE_SIZE;
+                CURRENT_PACKAGE_SIZE = 0;
+                ESP_ERROR_CHECK(esp_timer_start_once(oneshot_timer, 80/baud_rate));
+            }
+        }
+    }
+    free(dtmp);
+    dtmp = NULL;
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    messageQueue = xQueueCreate(5, sizeof(buffer));
+    if(messageQueue != NULL)
+    {
+        esp_log_level_set(TAG, ESP_LOG_INFO);
+        static const int baud_rate = 115200;
+
+        /* Configure parameters of an UART driver,
+        * communication pins and install the driver */
+        uart_config_t uart_config = {
+            .baud_rate = baud_rate,
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+            .source_clk = UART_SCLK_APB,
+        };
+        //Install UART driver, and get the queue.
+        uart_driver_install(EX_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart0_queue, 0);
+        uart_param_config(EX_UART_NUM, &uart_config);
+
+        //Set UART log level
+        esp_log_level_set(TAG, ESP_LOG_INFO);
+        //Set UART pins (using UART0 default pins ie no changes.)
+        uart_set_pin(EX_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+        //Set uart pattern detect function.
+        uart_enable_pattern_det_baud_intr(EX_UART_NUM, '+', PATTERN_CHR_NUM, 9, 0, 0); //PATTERN_CHR_NUM
+        //Reset the pattern queue length to record at most 20 pattern positions.
+        uart_pattern_queue_reset(EX_UART_NUM, 20);
+
+        //Create esp timer
+        const esp_timer_create_args_t oneshot_timer_args = {
+                .callback = &vTimerCallback,
+                /* argument specified here will be passed to timer callback function */
+                //.arg = (void*) periodic_timer,
+                .name = "timer"
+        };
+        esp_timer_handle_t oneshot_timer;
+        ESP_ERROR_CHECK(esp_timer_create(&oneshot_timer_args, &oneshot_timer));
+        
+        xTaskCreate(process_buffer_task, "process_buffer_task", 6000, NULL, 12, &process_buffer_Handle);
+        //Create a task to handler UART event from ISR
+        xTaskCreate(uart_event_task, "uart_event_task", 6000, (void*)oneshot_timer, 12, NULL);
+    }
+    else
+    {
+        //erro. tratar
+    }
+}
